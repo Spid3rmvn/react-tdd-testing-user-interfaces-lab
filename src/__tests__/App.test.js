c1666152397b9da3202e6160a92f1064a9ac26f6
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-01-29 21:30:14 +0300

    build(deps): update package-lock.json, package.json, App.js and App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,67 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
-
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  render(<App />);
+  console.log(screen.debug());  // Debugging output
+
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("displays an image of yourself", () => {
+  render(<App />);
+  console.log(screen.debug());  // Debugging output
+
+  const image = screen.getByAltText("My profile pic");
+
+  expect(image).toHaveAttribute("src", "https://via.placeholder.com/350");
+});
+
+test("displays second-level heading with the text `About Me`", () => {
+  render(<App />);
+  console.log(screen.debug());  // Debugging output
+
+  const secondLevelHeading = screen.getByRole("heading", {
+    name: /about me/i,
+    level: 2,
+  });
+
+  expect(secondLevelHeading).toBeInTheDocument();
+});
+
+test("displays a paragraph for your biography", () => {
+  render(<App />);
+  console.log(screen.debug());  // Debugging output
+
+  const bio = screen.getByText(/lorem ipsum/i);
+
+  expect(bio).toBeInTheDocument();
+});
+
+test("displays the correct links", () => {
+  render(<App />);
+  console.log(screen.debug());  // Debugging output
+
+  const githubLink = screen.getByRole("link", {
+    name: /github/i,
+  });
+  const linkedinLink = screen.getByRole("link", {
+    name: /linkedin/i,
+  });
+
+  expect(githubLink).toHaveAttribute(
+    "href",
+    expect.stringContaining("https://github.com")
+  );
+
+  expect(linkedinLink).toHaveAttribute(
+    "href",
+    expect.stringContaining("https://linkedin.com")
+  );
+});
